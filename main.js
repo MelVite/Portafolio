7bf42aad33db8fe18fdd0f9490245eb8bf8daf51
Melanie Vite <140591272+MelVite@users.noreply.github.com>
2023-07-30 23:23:53 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Desarrollador front-end jr de sitios web con HTML, CSS & Bootstrap')
   .pauseFor(200)
   .deleteChars(10)
   .start();
